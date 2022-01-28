4781356c57fce756fa48eef619517ad1a275dce7
Jota Landes <jhonatamalta@hotmail.com>
2022-01-28 13:28:15 -0300

    Complete Crud Actions created

--- Estudo-Dapper/Controllers/UsuarioController.cs
+++ Estudo-Dapper/Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
 ﻿
+using Estudo_Dapper.Models;
 using Estudo_Dapper.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,6 +26,50 @@ namespace Estudo_Dapper.Controllers
         {
            var user = await repository_Usuario.GetById(id);
            return View(user);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateView(UsuarioModel usuario)
+        {
+            await repository_Usuario.EditarUsuario(usuario);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateView(int id)
+        {
+            var user = await repository_Usuario.GetById(id);
+            return View(user);
+        }
+
+     
+
+        [HttpGet]
+        public IActionResult CriarView()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CriarView(UsuarioModel usuario)
+        {
+            
+            await repository_Usuario.CriarUsuario(usuario);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeletarView(int id)
+        {
+            var userToDel = await repository_Usuario.GetById(id);
+
+            return View(userToDel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeletarUser(int id)
+        {
+           await repository_Usuario.DeleteUsuario(id);
+
+            return RedirectToAction("Index");
         }
     }
 }
--- Estudo-Dapper/Services/Repository_Usuario.cs
+++ Estudo-Dapper/Services/Repository_Usuario.cs
@@ -6,6 +6,9 @@ namespace Estudo_Dapper.Services
 {
     public interface IRepository_Usuario
     {
+        Task CriarUsuario(UsuarioModel usuario);
+        Task DeleteUsuario(int id);
+        Task EditarUsuario(UsuarioModel usuario);
         Task<IEnumerable<UsuarioModel>> GetAllUser();
         Task<UsuarioModel> GetById(int id);
     }
@@ -29,6 +32,26 @@ namespace Estudo_Dapper.Services
             return await connection.QueryFirstOrDefaultAsync<UsuarioModel>(@"Select * from Usuarios where Id = @id", new { Id = id });
         }
 
+        public async Task CriarUsuario(UsuarioModel usuario)
+        {
+            using var connection = new SqlConnection(connectionString);
+            usuario.Id = await connection.QuerySingleAsync<int>(@"INSERT INTO  Usuarios (Nome,Email,Sexo,RG,CPF,NomeMae,SituacaoCadastro,DataCadastro)
+                                                VALUES (@Nome,@Email,@Sexo,@RG,@CPF,@NomeMae,@SituacaoCadastro,@DataCadastro);
+                                                SELECT SCOPE_IDENTITY()", usuario);
+
+           
+
+        }
+        public async Task EditarUsuario(UsuarioModel usuario)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(@"UPDATE USUARIOS SET Nome = @Nome,Email=@Email,Sexo=@Sexo,RG = @RG,CPF = @CPF,NomeMae=@NomeMae,SituacaoCadastro=@SituacaoCadastro,DataCadastro=@DataCadastro where ID = @Id ", usuario);
+        }
+         public async Task DeleteUsuario(int id)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(@"DELETE FROM Usuarios where Id = @Id", new { id });
+        }
        
     }
 }
